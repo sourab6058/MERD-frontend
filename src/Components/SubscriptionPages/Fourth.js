c1c7a03d50394d430c83e6c28c8eceb956a13093
sourab6058 <sourab1236kumar@gmail.com>
2021-09-26 10:50:18 +0530

    changes to plans table

--- src/Components/SubscriptionPages/Fourth.js
+++ src/Components/SubscriptionPages/Fourth.js
@@ -1,144 +1,210 @@
-import React, { useEffect, useState } from 'react';
-import Skeleton from '@material-ui/lab/Skeleton';
-import ResetIcon from '@material-ui/icons/Cached';
-import { Modal } from "antd"
-import { Menu, Dropdown } from 'antd';
+import React, { useEffect, useState } from "react";
+import Skeleton from "@material-ui/lab/Skeleton";
+import ResetIcon from "@material-ui/icons/Cached";
+import { Modal } from "antd";
+import { Menu, Dropdown } from "antd";
 import axios from "axios";
 import Aos from "aos";
 
 import "../../css/SubscriptionPages/styles.css";
-import { sortZones } from '../../utils/sort';
-import { Checkbox, Button } from 'antd';
-import SubcriptionPlansTable from './SubcriptionPlansTable';
+import { sortZones } from "../../utils/sort";
+import { Checkbox, Button } from "antd";
+import SubcriptionPlansTable from "./SubcriptionPlansTable";
 
 const citiesChecked = [];
 const categoriesChecked = [];
 
-function handleCheck(e, item, list) {
-    if (e.target.checked) {
-        list.push(item)
-    }
-    else {
-        const idx = list.indexOf(item);
-        if (idx > -1) {
-            list.splice(idx, 1);
-        }
-    }
-    console.log(list);
-}
-
 function SelectedItems() {
-    return (
-        <div style={{ position: "flex", flexDirection: "row" }}>
-            <div>
-                <h3>Selected cities</h3>
-                <ul style={{ marginLeft: "2rem" }}>
-                    {citiesChecked.map((city, idx) => (<li>{city}</li>))}
-                </ul>
-            </div>
-            <div>
-                <h3>Selected categories</h3>
-                <ul style={{ marginLeft: "2rem" }}>
-                    {categoriesChecked.map((cat) => (<li>{cat}</li>))}
-                </ul>
-            </div>
-            <h4>Total subscriptions:{" " + citiesChecked.length * categoriesChecked.length}</h4>
-        </div>
-    );
+  return (
+    <div style={{ position: "flex", flexDirection: "row" }}>
+      <div>
+        <h3>Selected cities</h3>
+        <ul style={{ marginLeft: "2rem" }}>
+          {citiesChecked.map((city, idx) => (
+            <li>{city}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Selected categories</h3>
+        <ul style={{ marginLeft: "2rem" }}>
+          {categoriesChecked.map((cat) => (
+            <li>{cat}</li>
+          ))}
+        </ul>
+      </div>
+      <h4>
+        Total subscriptions:
+        {" " + citiesChecked.length * categoriesChecked.length}
+      </h4>
+    </div>
+  );
 }
 
-const CityMenu = ({ options }) => {
-    return (options ? options[4][1].map((ele) => (<Menu>
-        <Menu.Item style={{ backgroundColor: "white" }} key={ele.id}>
-            <Checkbox style={{ fontSize: "1.25rem" }} key={ele.city} onChange={(e) => handleCheck(e, ele.city, citiesChecked)} >{ele.city}</Checkbox>
-        </Menu.Item>
-    </Menu>)) : (<p>Please Wait...</p>));
-}
+const Third = ({ setShowPlansTable, setSubscriptionsCount }) => {
+  const [options, setOptions] = useState(false);
+  const [openProceedModal, setOpenProceedModal] = useState(false);
+  const [openInvalid, setOpenInvalid] = useState(false);
+  const API_URL = "http://3.108.159.143:8000/api/filter";
 
-const CategoryMenu = ({ options }) => {
-    return (options ? options[3][1].map((ele) => (<Menu>
-        <Menu.Item style={{ backgroundColor: "white" }} key={ele.id}>
-            <Checkbox style={{ fontSize: "1.25rem" }} key={ele.name} onChange={(e) => handleCheck(e, ele.name, categoriesChecked)}>{ele.name}</Checkbox>
-        </Menu.Item>
-    </Menu>)) : (<p>Please Wait...</p>));
-}
-
-const Third = ({ setShowPlansTable }) => {
-    const [options, setOptions] = useState(false);
-    const [openProceedModal, setOpenProceedModal] = useState(false);
-    const [openInvalid, setOpenInvalid] = useState(false);
-    const API_URL = "http://3.108.159.143:8000/api/filter";
-
-    useEffect(() => {
-        Aos.init({ duration: 1000, });
-        axios
-            .get(API_URL)
-            .then((res) => {
-                let optionData = Object.entries(res.data.filters[0]);
-                optionData = sortZones(optionData);
-                setOptions(optionData);
-                console.log("options", options);
-            }, [options])
-            .catch((err) => {
-                console.log(err);
-            });
-    })
-
-
-    function proceedAfterSelections() {
-        if (citiesChecked.length && categoriesChecked.length) {
-            setOpenProceedModal(true);
-        }
-        else setOpenInvalid(true);
+  function handleCheck(e, item, list) {
+    if (e.target.checked) {
+      list.push(item);
+    } else {
+      const idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+      }
     }
+    setSubscriptionsCount(categoriesChecked.length * citiesChecked.length);
+  }
 
-    return (
-        <>
-            {openProceedModal &&
-                <Modal
-                    open={openProceedModal}
-                    visible={openProceedModal}
-                    onOk={() => setShowPlansTable(true)}
-                    onCancel={() => setOpenProceedModal(false)}>
+  const CategoryMenu = ({ options }) => {
+    return options ? (
+      options[3][1].map((ele) => (
+        <Menu>
+          <Menu.Item style={{ backgroundColor: "white" }} key={ele.id}>
+            <Checkbox
+              style={{ fontSize: "1.25rem" }}
+              key={ele.name}
+              onChange={(e) => handleCheck(e, ele.name, categoriesChecked)}
+            >
+              {ele.name}
+            </Checkbox>
+          </Menu.Item>
+        </Menu>
+      ))
+    ) : (
+      <p>Please Wait...</p>
+    );
+  };
 
-                    <SelectedItems />
+  const CityMenu = ({ options }) => {
+    return options ? (
+      options[4][1].map((ele) => (
+        <Menu>
+          <Menu.Item style={{ backgroundColor: "white" }} key={ele.id}>
+            <Checkbox
+              style={{ fontSize: "1.25rem" }}
+              key={ele.city}
+              onChange={(e) => handleCheck(e, ele.city, citiesChecked)}
+            >
+              {ele.city}
+            </Checkbox>
+          </Menu.Item>
+        </Menu>
+      ))
+    ) : (
+      <p>Please Wait...</p>
+    );
+  };
 
-                </Modal>}
-            {openInvalid &&
-                <Modal
-                    open={openInvalid}
-                    visible={openInvalid}
-                    onOk={() => { setOpenInvalid(false); setShowPlansTable(true); }}
-                    onCancel={() => setOpenInvalid(false)}>
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+    axios
+      .get(API_URL)
+      .then(
+        (res) => {
+          let optionData = Object.entries(res.data.filters[0]);
+          optionData = sortZones(optionData);
+          setOptions(optionData);
+          console.log("options", options);
+        },
+        [options]
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-                    {/* <h3>Select atleast one city and one category.</h3> */}
-                    <h3>Showing fake table for testing purposes.</h3>
+  function proceedAfterSelections() {
+    if (citiesChecked.length && categoriesChecked.length) {
+      setOpenProceedModal(true);
+    } else setOpenInvalid(true);
+  }
 
-                </Modal>}
-            <div className="option-main"><div className="sub-option-container">
-                Now, lets get started with the selections.
-                Choose the city and category you want to subscribe.
-                <div className="title-dd-section">
-                    {options ? (<h5 className="sub-dopdowns-title" style={{ color: "white" }}>Select below</h5>)
-                        : (<h5 className="sub-dopdowns-title" style={{ color: "white" }}>Please wait...</h5>)}
+  return (
+    <>
+      {openProceedModal && (
+        <Modal
+          open={openProceedModal}
+          visible={openProceedModal}
+          onOk={() => setShowPlansTable(true)}
+          onCancel={() => setOpenProceedModal(false)}
+        >
+          <SelectedItems />
+        </Modal>
+      )}
+      {openInvalid && (
+        <Modal
+          open={openInvalid}
+          visible={openInvalid}
+          onOk={() => {
+            setOpenInvalid(false);
+            setShowPlansTable(true);
+          }}
+          onCancel={() => setOpenInvalid(false)}
+        >
+          {/* <h3>Select atleast one city and one category.</h3> */}
+          <h3>Showing fake table for testing purposes.</h3>
+        </Modal>
+      )}
+      <div className="option-main">
+        <div className="sub-option-container">
+          Now, lets get started with the selections. Choose the city and
+          category you want to subscribe.
+          <div className="title-dd-section">
+            {options ? (
+              <h5 className="sub-dopdowns-title" style={{ color: "white" }}>
+                Select below
+              </h5>
+            ) : (
+              <h5 className="sub-dopdowns-title" style={{ color: "white" }}>
+                Please wait...
+              </h5>
+            )}
 
-                    {options ? (<div className="sub-dropdowns">
-                        <Dropdown overlay={<CityMenu options={options} />}>
-                            <a onClick={(e) => e.preventDefault()} className="drop-links">CITIES</a>
-                        </Dropdown>
-                        <Dropdown overlay={<CategoryMenu options={options} />}>
-                            <a onClick={(e) => e.preventDefault()} className="drop-links">CATEGORIES</a>
-                        </Dropdown>
-                    </div>) : (<Skeleton height={300} width={500} style={{ backgroundColor: "white", marginInline: "1rem" }} />)}
-                    {options && (<span className="next-btn" style={{ fontSize: "1.25rem", alignSelf: "self-start", marginInline: "0.25rem", paddingInline: 10 }}>{<ResetIcon />}Reset</span>)}
-                </div>
-
-            </div>
-                <span className="next-btn" onClick={proceedAfterSelections}>Lets go</span>
-            </div>
-
-        </>
-    )
+            {options ? (
+              <div className="sub-dropdowns">
+                <Dropdown overlay={<CityMenu options={options} />}>
+                  <a onClick={(e) => e.preventDefault()} className="drop-links">
+                    CITIES
+                  </a>
+                </Dropdown>
+                <Dropdown overlay={<CategoryMenu options={options} />}>
+                  <a onClick={(e) => e.preventDefault()} className="drop-links">
+                    CATEGORIES
+                  </a>
+                </Dropdown>
+              </div>
+            ) : (
+              <Skeleton
+                height={300}
+                width={500}
+                style={{ backgroundColor: "white", marginInline: "1rem" }}
+              />
+            )}
+            {options && (
+              <span
+                className="next-btn"
+                style={{
+                  fontSize: "1.25rem",
+                  alignSelf: "self-start",
+                  marginInline: "0.25rem",
+                  paddingInline: 10,
+                }}
+              >
+                {<ResetIcon />}Reset
+              </span>
+            )}
+          </div>
+        </div>
+        <span className="next-btn" onClick={proceedAfterSelections}>
+          Lets go
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Third;
--- src/Components/SubscriptionPages/index.js
+++ src/Components/SubscriptionPages/index.js
@@ -1,45 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
+import First from "./First";
+import Second from "./Second";
+import Third from "./Third";
+import Fourth from "./Fourth";
 
-import First from "./First"
-import Second from "./Second"
-import Third from "./Third"
-import Fourth from "./Fourth"
-
-
-import Footer from '../Footer';
-import NavTwo from '../NavTwo';
-import SubcriptionPlansTable from './SubcriptionPlansTable';
+import Footer from "../Footer";
+import NavTwo from "../NavTwo";
+import SubcriptionPlansTable from "./SubcriptionPlansTable";
 
 const SubscriptionJourney = () => {
-    const [scrollX, setScrollX] = useState(0);
-    const [showPlansTable, setShowPlansTable] = useState(false);
+  const [scrollX, setScrollX] = useState(0);
+  const [showPlansTable, setShowPlansTable] = useState(false);
+  const [subscriptionsCount, setSubscriptionsCount] = useState(0);
 
-    function handleNext() {
-        if (scrollX > -400) setScrollX(scrollX - 100);
-    }
-    function handlePrev() {
-        showPlansTable && setShowPlansTable(false);
-        if (scrollX < 0) setScrollX(scrollX + 100);
-    }
+  function handleNext() {
+    if (scrollX > -400) setScrollX(scrollX - 100);
+  }
+  function handlePrev() {
+    showPlansTable && setShowPlansTable(false);
+    if (scrollX < 0) setScrollX(scrollX + 100);
+  }
 
-    return (
-        <div className="sub-main-container">
-            <NavTwo />
-            {(scrollX < 0) && (<ArrowBackIcon className="arrow-back" onClick={handlePrev} style={{ fontSize: "3.5rem", color: "white" }} />)}
-            <div className="subscriptions">
-                <div className="sub-container" style={{ left: `${scrollX}vw` }}>
-                    <First handleNext={handleNext} />
-                    <Second handleNext={handleNext} handlePrev={handlePrev} />
-                    <Third handleNext={handleNext} handlePrev={handlePrev} />
-                    <Fourth handlePrev={handlePrev} handleNext={handleNext} setShowPlansTable={setShowPlansTable} />
-                </div>
-            </div>
-            {showPlansTable && (<><h1>Subscription Plans</h1><SubcriptionPlansTable /></>)}
-            <Footer />
+  return (
+    <div className="sub-main-container">
+      <NavTwo />
+      {scrollX < 0 && (
+        <ArrowBackIcon
+          className="arrow-back"
+          onClick={handlePrev}
+          style={{ fontSize: "3.5rem", color: "white" }}
+        />
+      )}
+      <div className="subscriptions">
+        <div className="sub-container" style={{ left: `${scrollX}vw` }}>
+          <First handleNext={handleNext} />
+          <Second handleNext={handleNext} handlePrev={handlePrev} />
+          <Third
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+            setSubscriptionsCount={setSubscriptionsCount}
+          />
+          <Fourth setShowPlansTable={setShowPlansTable} />
         </div>
-    )
+      </div>
+      {showPlansTable && (
+        <>
+          <h1>Subscription Plans</h1>
+          <SubcriptionPlansTable subscriptionsCount={subscriptionsCount} />
+        </>
+      )}
+      <Footer />
+    </div>
+  );
 };
 
 export default SubscriptionJourney;
