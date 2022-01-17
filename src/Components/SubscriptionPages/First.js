d5bc66e9d3553ec42135d9e5304de87f0d5cb649
sourab6058 <ragpareek796@gmail.com>
2022-01-17 22:49:52 +0530

    changes

--- src/Components/SubscriptionPages/First.js
+++ src/Components/SubscriptionPages/First.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 import "../../css/SubscriptionPages/styles.css";
+import fisrtImg from "../../img/handshake.jpg";
 
 const First = ({ handleNext }) => (
   <>
     <div className="sub-hero first">
+    <div className="sub-content">
+      <div className="sub-text">
       <span className="sub-hero-text">We understand that you would like to subscribe to our data. <br /> Great!</span>
-      <span className="next-btn" onClick={handleNext}>
+      <div className="next-btn-div"><span className="next-btn" onClick={handleNext}>
         Next
       </span>
+      </div>
+      </div >
+      <div className="handshake-img-div">
+      <img className="handshake-img"  src={fisrtImg} alt="handshake" />
+      </div>
+      </div>
     </div>
   </>
 );
